77e358caa1493b54f0f50800ad77aaefd413f09c
yizazz <jesusastorga53@gmail.com>
2025-10-25 23:58:24 +0000

    Listo para entrega

--- src/app.py
+++ src/app.py
@@ -13,10 +13,17 @@ def todo():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json
-    print("Incoming request with the following body", request_body)
+    request_body = request.get_json(force=True)
+    print("Incoming request with the following body")
     todos.append(request_body)
-    return 'Response for the POST todo'
+    return jsonify(todos)
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    print("This is the position to delete:", position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
